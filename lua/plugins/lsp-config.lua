b32bb7ee545677abe54ff000f570828741c87507
Lyon Liang <lyon.liang12@gmail.com>
2026-06-23 13:55:47 +0800

    feat(lsp): add cscope-style navigation keymaps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -44,10 +44,43 @@ return {
 
       vim.lsp.enable(servers)
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(event)
+          local bufnr = event.buf
+          local builtin = require("telescope.builtin")
+          local map = function(mode, lhs, rhs, desc)
+            vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+          end
+          local supports_method = function(method)
+            for _, client in ipairs(vim.lsp.get_clients({ bufnr = bufnr })) do
+              if client:supports_method(method, bufnr) then
+                return true
+              end
+            end
+            return false
+          end
+          local declaration_or_definition = function()
+            if supports_method("textDocument/declaration") then
+              vim.lsp.buf.declaration()
+            else
+              builtin.lsp_definitions()
+            end
+          end
+
+          map("n", "K", vim.lsp.buf.hover, "Hover")
+          map("n", "gD", declaration_or_definition, "Goto Declaration")
+          map("n", "gd", builtin.lsp_definitions, "Goto Definition")
+          map("n", "gr", builtin.lsp_references, "Goto References")
+          map("n", "gi", builtin.lsp_implementations, "Goto Implementation")
+          map("n", "gy", builtin.lsp_type_definitions, "Goto Type Definition")
+          map("n", "<leader>ds", builtin.lsp_document_symbols, "Document Symbols")
+          map("n", "<leader>ws", builtin.lsp_workspace_symbols, "Workspace Symbols")
+          map("n", "<leader>ci", builtin.lsp_incoming_calls, "Incoming Calls")
+          map("n", "<leader>co", builtin.lsp_outgoing_calls, "Outgoing Calls")
+          map("n", "<leader>rn", vim.lsp.buf.rename, "Rename Symbol")
+          map("n", "<leader>ca", vim.lsp.buf.code_action, "Code Action")
+        end,
+      })
     end
   }
 }
